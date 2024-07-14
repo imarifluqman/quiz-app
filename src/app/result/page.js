f894ebc11f17e24bfbf7cd85f399519415cf4402
Muhammad Arif <arifluqman477@gmail.com>
2024-07-15 04:13:21 +0500

    create certificate responcse

--- src/app/result/page.js
+++ src/app/result/page.js
@@ -6,6 +6,7 @@ import Certificate from "../../../public/Certificate.png";
 import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 import Loader from "../components/loader/Loader";
+
 export default function Page(params) {
   let [isLoading, setLoading] = useState(true);
   const router = useRouter();
@@ -35,14 +36,13 @@ export default function Page(params) {
       // mm stand for millimeters, in for inches
       // a4 stand for A4 page size
       const pdf = new jsPDF("l", "mm", "a4", true);
-      console.log(pdf);
       const pdfWidth = pdf.internal.pageSize.getWidth();
       const pdfHeight = pdf.internal.pageSize.getHeight();
       const imgWidth = canvas.width;
       const imgHeight = canvas.height;
       const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
       const imgX = (pdfWidth - imgWidth * ratio) / 2;
-      const imgY = 30;
+      const imgY = 0;
 
       pdf.addImage(
         imgData,
@@ -52,54 +52,52 @@ export default function Page(params) {
         imgWidth * ratio,
         imgHeight * ratio
       );
+      pdf.text(data.rollNo, 230, 63);
+      pdf.text(data.name.toUpperCase(), 146, 108);
+      // pdf.addFont("Poppins", "Poppins", "normal");
+      pdf.text(data.course.toUpperCase(), 150, 128);
+      pdf.text(data.score.toString() + "%", 220, 128);
       pdf.save(`GIAIC-${data.name}.pdf`);
     });
   };
 
   return (
-    <>
-      <div
-        ref={pdfRef}
-        className=" w-[100%] h-[100vh] flex justify-center items-center"
-      >
-        {isLoading ? (
+    <div
+      // ref={pdfRef}
+      className=" w-[100%] h-[100vh] flex flex-col justify-center  items-center"
+    >
+      {isLoading ? (
+        <div className=" w-[100%] h-[100vh] flex justify-center items-center">
           <Loader />
-        ) : (
-          <>
-            <div className="w-[70%] h-[100%] mx-auto">
-              <Image
-                className=" relative  top-[50%] right-[50%] translate-x-[50%] translate-y-[-50%] "
-                src={Certificate}
-                width={1200}
-                height={1200}
-                priority={true}
-                alt="Governor Sindh certificate Image"
-              />
+        </div>
+      ) : (
+        <>
+          <div className="">
+            <p className="">Name : {data.name}</p>
+            <p className="">Roll No : {data.rollNo}</p>
+            <p className="">Course : {data.course}</p>
+            <p className=" "> Your Score :{data.score}%</p>
+          </div>
+          <Image
+            ref={pdfRef}
+            className="h-auto w-auto "
+            src={Certificate}
+            width={800}
+            height={800}
+            priority={true}
+            alt="Governor Sindh certificate Image"
+          />
+        </>
+      )}
 
-              <p className="absolute top-[150px] right-[350px]  text-[12px] z-1 text-black  ">
-                {data.rollNo}
-              </p>
-
-              <p className="absolute top-[290px] right-[50%] translate-x-[50%] translate-y-[-50%] text-3xl text-bold z-1 text-black  ">
-                {data.name}
-              </p>
-
-              <p className="absolute bottom-[235px] right-[580px]  uppercase text-1xl text-bold z-1 text-black  ">
-                {data.course}
-              </p>
-              <p className="absolute bottom-[235px] right-[400px] text-1xl text-bold z-1 text-black  ">
-                {data.score}%
-              </p>
-            </div>
-          </>
-        )}
-      </div>
-      <button
-        className="bg-green-600 text-white p-2 mt-6 mx-auto block z-10"
-        onClick={downloadPdf}
-      >
-        Download your Certificate
-      </button>
-    </>
+      {!isLoading ? (
+        <button
+          className="bg-green-600 text-white px-2 py-1 mx-auto block mt-1"
+          onClick={downloadPdf}
+        >
+          Download Certificate
+        </button>
+      ) : null}
+    </div>
   );
 }
