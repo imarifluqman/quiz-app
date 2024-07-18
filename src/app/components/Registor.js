4f7e91b415b9940bb5dc18b03219a7759d28b729
Muhammad Arif <64982848+imarifluqman@users.noreply.github.com>
2024-07-18 16:52:26 +0500

    Logo changed

--- src/app/components/Registor.js
+++ src/app/components/Registor.js
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import logo from "../../../public/logo.png";
+import logo from "../../../public/Logo.png";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Loader from "./loader/Loader";
