43bdc49246238cc8bbfec99a18db45f8c1ecd2f2
Muhammad Arif <arifluqman477@gmail.com>
2024-07-23 00:52:30 +0500

    bug again

--- src/app/components/schemas/index.js
+++ src/app/components/schemas/index.js
@@ -4,5 +4,5 @@ export const schema = yup.object({
   name: yup.string().min(3).required(),
   fatherName: yup.string().min(3).required("Father name is required"),
   email: yup.string().email().required(),
-  phone: yup.number(11).required(),
+  phone: yup.number().max(11).required(),
 });
