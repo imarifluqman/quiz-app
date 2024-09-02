fd90a1616ad35ff396956b71e89a4362990e5573
Muhammad Arif <arifluqman477@gmail.com>
2024-09-03 00:13:41 +0500

    finale touch

--- src/app/components/videoComponent.jsx
+++ src/app/components/videoComponent.jsx
@@ -2,9 +2,9 @@ function VideoComponent({ data }) {
   return (
     <>
       <iframe
-        className="w-[auto]"
-        width="660"
-        height="415"
+        className="w-[350px]  lg:w-[660px] lg:h-[415px] sm:w-[660px] sm:h-[415px] mx-auto"
+        // width="660"
+        // height="415"
         src={`https://www.youtube.com/embed/${data}`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
