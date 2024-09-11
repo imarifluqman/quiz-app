03ae06b7add1975ec4f192344dfa78e186a0027e
Muhammad Arif <64982848+imarifluqman@users.noreply.github.com>
2024-09-11 10:58:01 +0500

    Update testimonal.jsx

--- src/app/components/testimonal.jsx
+++ src/app/components/testimonal.jsx
@@ -71,6 +71,26 @@ function Testimonal(params) {
           </div>
         </div>
       </div>
+      <div className="lg:w-[400px] w-[100%] mx-auto mt-6">
+        <div className="border rounded m-2 p-4 flex justify-center items-center  bg-slate-100">
+          <div className="w-[30%] mr-4">
+            <Image
+              src={Next}
+              alt="News and Blogs"
+              width={100}
+              height={100}
+              className="rounded-full "
+            />
+            <p className="text-[14px] mt-2 text-center">Arif Luqman</p>
+          </div>
+          <div className="w-[70%] ">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Perferendis est fuga ullam qui dicta quas accusantium odit,
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
